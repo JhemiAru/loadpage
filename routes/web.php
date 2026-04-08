7afab3312dfc9105e21956c737858ce850f00500
Jhemi Aru <jhemiaruquipacondori@gmail.com>
2026-04-08 12:54:12 -0400

    Base de datos de empresas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('empresas', EmpresaController::class);
+
+DB::table('empresas')->insert([
+    'nombre' => 'Cafe felicidad',
+    'descripcion' => 'Cafe en El Alto',
+    'descuento' => 5.0
+]);
